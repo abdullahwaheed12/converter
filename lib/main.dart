683ec4a67de656f1f01afc0364abf36ea76ab60d
abdullahwaheed <pgcbwp12@gmail.com>
2023-08-15 12:10:14 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
